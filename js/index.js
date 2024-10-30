44674e83344e3352723200582f313d5d4953eefa
Meiir <a12790958@gmail.com>
2024-10-30 19:21:10 +0500

    added css, js, html

---
+++ js/index.js
@@ -0,0 +1,32 @@
+//string
+var name = "Satybaldy"
+
+// number
+const PI = 3.14
+
+let aty;
+// aty = "Meir"
+
+console.log("Аты:" + aty)
+
+console.log(PI)
+
+var sum = null; //Мәні бос
+console.log("Sum:" + sum)
+
+var san1 = 10
+var san2 = 15
+
+sum = san1 + san2
+console.log("Sum:", sum)
+
+//boolean true / false
+if(true){
+    var name = "Galymbek"
+    console.log("Шекара ішіндегі аты" + name);
+
+    if(true){
+        var name = "Nurs"
+        console.log("Шекара ішіндегі ішіндегісі аты" + name)
+    }
+}
