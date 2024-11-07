10b67367066f75aa8be9da24e1e3322b1b195cad
Meiir <a12790958@gmail.com>
2024-11-07 17:39:15 +0500

    added js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ let num = prompt('0 мен 99 арасындағы санды енгізіңіз
 if(num >= 0 && num <= 9) {
     alert('Сан:' + num)
 }else if(num >= 10 && num <= 99) {
-  let Num1 = prompt(num / 10);
-  let Num2 = Math.floor(num % 10)
+  let Num1 = Math.floor(num / 10);
+  let Num2 = (num % 10)
     alert('Бірінші сан:' + Num1 + '\n' + 'Екінші сан:' + Num2);
 }else{
     alert('Тек 0 мен 99 арасындағы санды енгізіңіз.');
