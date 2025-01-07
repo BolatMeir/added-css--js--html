f9c1983344d8c4431cde48dc3b5f6aa81b3f9193
Meiir <a12790958@gmail.com>
2025-01-07 14:16:58 +0500

    added html and css

--- js/index.js
+++ js/index.js
@@ -1,16 +1,86 @@
 
-let num = prompt('0 мен 99 арасындағы санды енгізіңіз:')
-if(num >= 0 && num <= 9) {
-    alert('Сан:' + num)
-}else if(num >= 10 && num <= 99) {
-  let Num1 = Math.floor(num / 10);
-  let Num2 = (num % 10)
-    alert('Бірінші сан:' + Num1 + '\n' + 'Екінші сан:' + Num2);
-}else{
-    alert('Тек 0 мен 99 арасындағы санды енгізіңіз.');
+// let num = prompt('0 мен 99 арасындағы санды енгізіңіз:')
+// if(num >= 0 && num <= 9) {
+//     alert("Сан:" + num)
+// }else if(num >= 10 && num <= 99) {
+//   let Num1 = parseInt(num / 10);
+//   let Num2 = (num % 10)
+//     alert("Бірінші сан:" + Num1 + "\n" + "Екінші сан:" + Num2);
+// }else{
+//     alert("Тек 0 мен 99 арасындағы санды енгізіңіз.");
+// }
+
+
+
+
+
+let susin = prompt("Qandai susin turin tandaisiz: chai/coffee/su")
+switch(susin){
+    case "coffee":
+        alert("Sen coffee turin tandadiniz")
+        break;
+    case "chai":
+        alert("Sen chai turin tandadiniz")
+        break;
+    case "su":
+        alert("Sen su turin tandadiniz")
+        break;
+    default:
+        alert("Bizde ondai susin zhok!Qaitasin tandaniz")
+}
+// let turi = prompt("Qandai sirop tandaisiz: qaramelni/checolatni/qlubnichni")
+switch(susin){
+    case "qaramelni":
+        alert("Sen qaramel turin tandadiniz")
+        break;
+    case "checolatni":
+        alert("Sen checolat turin tandadiniz")
+        break;
+    case "qlubnichni":
+        alert("Sen qlubnich turin tandadiniz")
+    default:
+        alert("Bizde ondai sirop zhok!Qaitasin tandaniz")
+}
+switch(susin){
+    case "":
 }
 
 
 
 
+// alert("Burger turin tanda Burger standart/Chezburger:")
+// let burger = prompt("Burger turin zhaz:")
+// let shtuk = +prompt("kansha shtuk")
+// let sum = 0;
+// switch (burger) {
+//     case "Burger standart":
+//         alert("Sen Burger standart turin tandadyn")
+//         sum += 1090
+//         break;
+//     case "Chezburger":
+//         alert("Sen Chezburger turin tandadyn")
+//         sum += 1190
+//         break;
+//     default:
+//         alert("Сыз бургерды тандамадынныз кийта тандауынызды сураймын!")
+// }
 
+// alert("Сыздын заказ кабылданды" + '\n' + "Бургер туры: " + burger + '\n' + "Суммасы: " + sum * shtuk)
+
+
+
+
+
+
+
+// (burger) {
+//     sum += 1190
+// }
+// else if (burger == "Двойной бургер" || burger == "двойной бургер") {
+//     sum += 1990
+// }
+// else {
+//     alert("Сыз бургерды тандамадынныз кийта тандауынызды сураймын")
+// }
+// alert("Сыздын заказ кабылданды" + '\n' + "Бургер туры: " + burger + '\n' + "Суммасы: " + sum * shtuk)
+// burger == "Гамбургер" || burger == "гамбургер"
